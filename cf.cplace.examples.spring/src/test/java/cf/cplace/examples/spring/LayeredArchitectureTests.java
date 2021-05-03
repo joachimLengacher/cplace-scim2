e668d1ef41711e5e8690b6ec5f353642a26fc81f
Joachim Lengacher <joachim.lengacher@collaboration-factory.de>
2021-05-03 12:45:11 +0200

    Changed it to using ArchUnit annotations.

--- cf.cplace.examples.spring/src/test/java/cf/cplace/examples/spring/LayeredArchitectureTests.java
+++ cf.cplace.examples.spring/src/test/java/cf/cplace/examples/spring/LayeredArchitectureTests.java
@@ -1,26 +1,26 @@
 package cf.cplace.examples.spring;
 
-import com.tngtech.archunit.core.domain.JavaClasses;
-import com.tngtech.archunit.core.importer.ClassFileImporter;
+import cf.cplace.platform.archunit.ArchUnitRunner;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
-import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
 
+@RunWith(ArchUnitRunner.class)
+@AnalyzeClasses(packages = "cf.cplace.examples.spring")
 public class LayeredArchitectureTests {
 
-    @Test
-    public void layer_dependencies_are_respected() {
-        JavaClasses importedClasses = new ClassFileImporter().importPackages("cf.cplace.examples.spring");
-        ArchRule myRule = layeredArchitecture()
-                .layer("Infrastructure").definedBy("..cf.cplace.examples.spring.assembly..")
-                .layer("Adapters").definedBy("..cf.cplace.examples.spring.adapter..")
-                .layer("Application Logic").definedBy("..cf.cplace.examples.spring.usecase..")
-                .layer("Domain").definedBy("..cf.cplace.examples.spring.domain..")
-                .whereLayer("Infrastructure").mayNotBeAccessedByAnyLayer()
-                .whereLayer("Adapters").mayOnlyBeAccessedByLayers("Infrastructure")
-                .whereLayer("Application Logic").mayOnlyBeAccessedByLayers("Infrastructure", "Adapters")
-                .whereLayer("Domain").mayOnlyBeAccessedByLayers("Infrastructure", "Application Logic", "Adapters");
-        myRule.check(importedClasses);
-    }
+    @ArchTest
+    public static final ArchRule platformUsageRules = layeredArchitecture()
+            .layer("Infrastructure").definedBy("..cf.cplace.examples.spring.assembly..")
+            .layer("Adapters").definedBy("..cf.cplace.examples.spring.adapter..")
+            .layer("Application Logic").definedBy("..cf.cplace.examples.spring.usecase..")
+            .layer("Domain").definedBy("..cf.cplace.examples.spring.domain..")
+            .whereLayer("Infrastructure").mayNotBeAccessedByAnyLayer()
+            .whereLayer("Adapters").mayOnlyBeAccessedByLayers("Infrastructure")
+            .whereLayer("Application Logic").mayOnlyBeAccessedByLayers("Infrastructure", "Adapters")
+            .whereLayer("Domain").mayOnlyBeAccessedByLayers("Infrastructure", "Application Logic", "Adapters");
+
 }
