2f63d2e66a197172629fc12b32c0ea046537d549
Daniel Kreck <daniel.kreck@collaboration-factory.de>
2021-05-17 18:11:29 +0200

    Implement PluginBeanActivationProcessor which removes beans of disabled plugins

--- cf.cplace.examples.spring/src/main/java/cf/cplace/examples/spring/assembly/PluginSpringConfiguration.java
+++ cf.cplace.examples.spring/src/main/java/cf/cplace/examples/spring/assembly/PluginSpringConfiguration.java
@@ -1,16 +1,11 @@
 package cf.cplace.examples.spring.assembly;
 
-import cf.cplace.examples.spring.SpringPlugin;
 import cf.cplace.examples.spring.adapter.cplace.CplaceDirectorRepository;
 import cf.cplace.examples.spring.adapter.cplace.CplaceMovieRepository;
-import cf.cplace.examples.spring.adapter.rest.DirectorResource;
-import cf.cplace.examples.spring.adapter.rest.GlobalExceptionHandler;
-import cf.cplace.examples.spring.adapter.rest.MovieResource;
 import cf.cplace.examples.spring.domain.port.DirectorRepository;
 import cf.cplace.examples.spring.domain.port.MovieRepository;
 import cf.cplace.examples.spring.usecase.impl.DirectorApplication;
 import cf.cplace.examples.spring.usecase.impl.MovieApplication;
-import cf.cplace.platform.api.spring.ConditionalOnActivatedPlugin;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -19,7 +14,6 @@ import org.springframework.context.annotation.Configuration;
  * This plugin's central Spring configuration that is picked up by cplace at start-up.
  */
 @Configuration
-@ConditionalOnActivatedPlugin(SpringPlugin.class)
 @ComponentScan("cf.cplace.examples.spring.adapter.rest")
 public class PluginSpringConfiguration {
 
@@ -41,5 +35,10 @@ public class PluginSpringConfiguration {
     @Bean
     public DirectorApplication directorApplication(DirectorRepository directorRepository) {
         return new DirectorApplication(directorRepository);
+    }
+
+    @Bean
+    public String genericStringBean() {
+        return "hello";
     }
 }
