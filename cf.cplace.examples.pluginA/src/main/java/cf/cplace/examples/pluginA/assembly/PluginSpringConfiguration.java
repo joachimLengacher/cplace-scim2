6b31cba1cd2593484811f9afca039dfc8d225c53
Daniel Kreck <daniel.kreck@collaboration-factory.de>
2021-06-29 11:57:33 +0200

    Added platform component

--- cf.cplace.examples.pluginA/src/main/java/cf/cplace/examples/pluginA/assembly/PluginSpringConfiguration.java
+++ cf.cplace.examples.pluginA/src/main/java/cf/cplace/examples/pluginA/assembly/PluginSpringConfiguration.java
@@ -2,6 +2,7 @@ package cf.cplace.examples.pluginA.assembly;
 
 import cf.cplace.examples.pluginA.api.Multiplier;
 import cf.cplace.examples.pluginA.impl.DefaultMultiplier;
+import cf.cplace.platform.api.configuration.CplaceConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,8 +10,8 @@ import org.springframework.context.annotation.Configuration;
 public class PluginSpringConfiguration {
 
     @Bean
-    public Multiplier multiplier() {
-        return new DefaultMultiplier();
+    public Multiplier multiplier(CplaceConfiguration cplaceConfiguration) {
+        return new DefaultMultiplier(cplaceConfiguration.isMultiTenancy());
     }
 
 }
--- cf.cplace.examples.pluginA/src/main/java/cf/cplace/examples/pluginA/impl/DefaultMultiplier.java
+++ cf.cplace.examples.pluginA/src/main/java/cf/cplace/examples/pluginA/impl/DefaultMultiplier.java
@@ -3,8 +3,19 @@ package cf.cplace.examples.pluginA.impl;
 import cf.cplace.examples.pluginA.api.Multiplier;
 
 public class DefaultMultiplier implements Multiplier {
+
+    private final boolean multiTenancy;
+
+    public DefaultMultiplier(boolean multiTenancy) {
+        this.multiTenancy = multiTenancy;
+    }
+
     @Override
     public long multiply(int a, int b) {
-        return a * b;
+        if(!multiTenancy) {
+            return a * b;
+        } else {
+            throw new IllegalStateException("Only possible in single tenant mode");
+        }
     }
 }
