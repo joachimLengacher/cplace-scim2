9c6406206592331093a2c5a00df4b59c6f144b46
Daniel Kreck <daniel.kreck@collaboration-factory.de>
2021-06-30 14:59:21 +0200

    Print pre destroy

--- cf.cplace.examples.pluginA/src/main/java/cf/cplace/examples/pluginA/assembly/PluginSpringConfiguration.java
+++ cf.cplace.examples.pluginA/src/main/java/cf/cplace/examples/pluginA/assembly/PluginSpringConfiguration.java
@@ -7,6 +7,8 @@ import cf.cplace.platform.api.spring.annotation.Exported;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.PreDestroy;
+
 @Configuration
 public class PluginSpringConfiguration {
 
@@ -14,6 +16,11 @@ public class PluginSpringConfiguration {
     @Bean("cf.cplace.examples.pluginA.multiplier")
     public Multiplier multiplier(CplaceConfiguration cplaceConfiguration) {
         return new DefaultMultiplier(cplaceConfiguration.isMultiTenancy());
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        System.out.println("Destroy Plugin A");
     }
 
 }
--- cf.cplace.examples.pluginB/src/main/java/cf/cplace/examples/pluginB/assembly/PluginSpringConfiguration.java
+++ cf.cplace.examples.pluginB/src/main/java/cf/cplace/examples/pluginB/assembly/PluginSpringConfiguration.java
@@ -2,6 +2,13 @@ package cf.cplace.examples.pluginB.assembly;
 
 import org.springframework.context.annotation.ComponentScan;
 
+import javax.annotation.PreDestroy;
+
 @ComponentScan("cf.cplace.examples.pluginB.rest")
 public class PluginSpringConfiguration {
+
+    @PreDestroy
+    public void preDestroy() {
+        System.out.println("Destroy Plugin B");
+    }
 }
