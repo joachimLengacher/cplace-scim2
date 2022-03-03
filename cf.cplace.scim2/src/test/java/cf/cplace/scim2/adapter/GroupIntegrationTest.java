a2b23cfa7ce7784e41b042c0f745b7574b2eb8e8
Joachim Lengacher <joachim.lengacher@collaboration-factory.de>
2022-03-03 09:30:56 +0100

    Split the integration test into two classes

---
+++ cf.cplace.scim2/src/test/java/cf/cplace/scim2/adapter/GroupIntegrationTest.java
@@ -0,0 +1,126 @@
+package cf.cplace.scim2.adapter;
+
+import cf.cplace.platform.test.util.StartServerRule;
+import com.unboundid.scim2.common.messages.ListResponse;
+import com.unboundid.scim2.common.types.GroupResource;
+import io.restassured.response.Response;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestRule;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
+
+public class GroupIntegrationTest {
+
+    public static final String GROUP_NAME = "The Simpsons";
+
+    @Rule
+    public TestRule startServer = new StartServerRule(this);
+
+    @Test
+    public void testGroupLifecycle() {
+        findAllGroups();
+        String id = createGroup();
+        findGroup(id);
+        updateGroup(id);
+    }
+
+    private void findAllGroups() {
+        Response response = given()
+                .auth()
+                .preemptive()
+                .basic("mustermann@test.tricia", "ottto")
+                .header("Content-type", "application/json")
+                .header("Accept", "application/scim+json; charset=utf-8")
+                .and()
+                .param("count", 30)
+                .param("startIndex", 1)
+                .when()
+                .get("http://localhost:8083/intern/tricia/cplace-api/cf.cplace.scim2/Groups")
+                .then()
+                .extract().response();
+
+        assertThat(response.statusCode(), is(OK.value()));
+        @SuppressWarnings("unchecked")
+        final ListResponse<GroupResource> allGroups = response.getBody().as(ListResponse.class);
+        assertThat(allGroups.getTotalResults(), is(1));
+        assertThat(allGroups.getItemsPerPage(), is(30));
+        assertThat(allGroups.getStartIndex(), is(0));
+        assertThat(response.jsonPath().getList("schemas"), contains("urn:ietf:params:scim:api:messages:2.0:ListResponse"));
+    }
+
+
+    private GroupResource findGroup(String id) {
+        Response response = given()
+                .auth()
+                .preemptive()
+                .basic("mustermann@test.tricia", "ottto")
+                .header("Content-type", "application/json")
+                .header("Accept", "application/scim+json; charset=utf-8")
+                .when()
+                .get("http://localhost:8083/intern/tricia/cplace-api/cf.cplace.scim2/Groups/" + id)
+                .then()
+                .extract().response();
+
+        assertThat(response.statusCode(), is(OK.value()));
+        assertGroupValues(response, GROUP_NAME);
+        assertThat(response.jsonPath().getString("id"), is(id));
+        return response.as(GroupResource.class);
+    }
+
+    private void updateGroup(String id) {
+        GroupResource group = findGroup(id);
+        group.setDisplayName("The Simpsons Family");
+
+        Response response = given()
+                .auth()
+                .preemptive()
+                .basic("mustermann@test.tricia", "ottto")
+                .header("Content-type", "application/json")
+                .header("Accept", "application/scim+json; charset=utf-8")
+                .and()
+                .body(group)
+                .when()
+                .put("http://localhost:8083/intern/tricia/cplace-api/cf.cplace.scim2/Groups/" + id)
+                .then()
+                .extract().response();
+
+        assertThat(response.statusCode(), is(OK.value()));
+        assertGroupValues(response, "The Simpsons Family");
+        assertThat(response.jsonPath().getString("id"), is(id));
+    }
+
+    private String createGroup() {
+        GroupResource group = new GroupResource()
+                .setDisplayName(GROUP_NAME);
+
+        Response response = given()
+                .auth()
+                .preemptive()
+                .basic("mustermann@test.tricia", "ottto")
+                .header("Content-type", "application/json")
+                .header("Accept", "application/scim+json; charset=utf-8")
+                .and()
+                .body(group)
+                .when()
+                .post("http://localhost:8083/intern/tricia/cplace-api/cf.cplace.scim2/Groups")
+                .then()
+                .extract().response();
+
+        assertThat(response.statusCode(), is(CREATED.value()));
+        assertGroupValues(response, GROUP_NAME);
+        assertThat(response.jsonPath().getString("id"), is(not(nullValue())));
+
+        return response.jsonPath().getString("id");
+    }
+
+    private void assertGroupValues(Response response, String name) {
+        assertThat(response.jsonPath().getList("schemas"), hasSize(1));
+        assertThat(response.jsonPath().getList("schemas"), contains("urn:ietf:params:scim:schemas:core:2.0:Group"));
+        assertThat(response.jsonPath().getString("displayName"), is(name));
+    }
+}
--- cf.cplace.scim2/src/test/java/cf/cplace/scim2/adapter/UserIntegrationTest.java
+++ cf.cplace.scim2/src/test/java/cf/cplace/scim2/adapter/UserIntegrationTest.java
@@ -3,7 +3,6 @@ package cf.cplace.scim2.adapter;
 import cf.cplace.platform.test.util.StartServerRule;
 import com.unboundid.scim2.common.messages.ListResponse;
 import com.unboundid.scim2.common.types.Email;
-import com.unboundid.scim2.common.types.GroupResource;
 import com.unboundid.scim2.common.types.Name;
 import com.unboundid.scim2.common.types.UserResource;
 import io.restassured.response.Response;
@@ -18,10 +17,9 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.springframework.http.HttpStatus.*;
 
-public class IntegrationTest {
+public class UserIntegrationTest {
 
     public static final String USER_NAME = "bart.simpson@cplace.io";
-    public static final String GROUP_NAME = "The Simpsons";
     public static final String GIVEN_NAME = "Bart";
     public static final String FAMILY_NAME = "Simpson";
 
@@ -36,14 +34,6 @@ public class IntegrationTest {
         findAllUsers();
         updateUser(id);
         deleteUser(id);
-    }
-
-    @Test
-    public void testGroupLifecycle() {
-        findAllGroups();
-        String id = createGroup();
-        findGroup(id);
-        updateGroup(id);
     }
 
     private String createUser() {
@@ -165,100 +155,5 @@ public class IntegrationTest {
         assertThat(response.jsonPath().getBoolean("active"), is(isActive));
         assertThat(response.jsonPath().getList("emails", Email.class).get(0).getPrimary(), is(true));
         assertThat(response.jsonPath().getList("emails", Email.class).get(0).getValue(), is(email));
-    }
-
-    private void findAllGroups() {
-        Response response = given()
-                .auth()
-                .preemptive()
-                .basic("mustermann@test.tricia", "ottto")
-                .header("Content-type", "application/json")
-                .header("Accept", "application/scim+json; charset=utf-8")
-                .and()
-                .param("count", 30)
-                .param("startIndex", 1)
-                .when()
-                .get("http://localhost:8083/intern/tricia/cplace-api/cf.cplace.scim2/Groups")
-                .then()
-                .extract().response();
-
-        assertThat(response.statusCode(), is(OK.value()));
-        @SuppressWarnings("unchecked")
-        final ListResponse<GroupResource> allGroups = response.getBody().as(ListResponse.class);
-        assertThat(allGroups.getTotalResults(), is(1));
-        assertThat(allGroups.getItemsPerPage(), is(30));
-        assertThat(allGroups.getStartIndex(), is(0));
-        assertThat(response.jsonPath().getList("schemas"), contains("urn:ietf:params:scim:api:messages:2.0:ListResponse"));
-    }
-
-
-    private GroupResource findGroup(String id) {
-        Response response = given()
-                .auth()
-                .preemptive()
-                .basic("mustermann@test.tricia", "ottto")
-                .header("Content-type", "application/json")
-                .header("Accept", "application/scim+json; charset=utf-8")
-                .when()
-                .get("http://localhost:8083/intern/tricia/cplace-api/cf.cplace.scim2/Groups/" + id)
-                .then()
-                .extract().response();
-
-        assertThat(response.statusCode(), is(OK.value()));
-        assertGroupValues(response, GROUP_NAME);
-        assertThat(response.jsonPath().getString("id"), is(id));
-        return response.as(GroupResource.class);
-    }
-
-    private void updateGroup(String id) {
-        GroupResource group = findGroup(id);
-        group.setDisplayName("The Simpsons Family");
-
-        Response response = given()
-                .auth()
-                .preemptive()
-                .basic("mustermann@test.tricia", "ottto")
-                .header("Content-type", "application/json")
-                .header("Accept", "application/scim+json; charset=utf-8")
-                .and()
-                .body(group)
-                .when()
-                .put("http://localhost:8083/intern/tricia/cplace-api/cf.cplace.scim2/Groups/" + id)
-                .then()
-                .extract().response();
-
-        assertThat(response.statusCode(), is(OK.value()));
-        assertGroupValues(response, "The Simpsons Family");
-        assertThat(response.jsonPath().getString("id"), is(id));
-    }
-
-    private String createGroup() {
-        GroupResource group = new GroupResource()
-                .setDisplayName(GROUP_NAME);
-
-        Response response = given()
-                .auth()
-                .preemptive()
-                .basic("mustermann@test.tricia", "ottto")
-                .header("Content-type", "application/json")
-                .header("Accept", "application/scim+json; charset=utf-8")
-                .and()
-                .body(group)
-                .when()
-                .post("http://localhost:8083/intern/tricia/cplace-api/cf.cplace.scim2/Groups")
-                .then()
-                .extract().response();
-
-        assertThat(response.statusCode(), is(CREATED.value()));
-        assertGroupValues(response, GROUP_NAME);
-        assertThat(response.jsonPath().getString("id"), is(not(nullValue())));
-
-        return response.jsonPath().getString("id");
-    }
-
-    private void assertGroupValues(Response response, String name) {
-        assertThat(response.jsonPath().getList("schemas"), hasSize(1));
-        assertThat(response.jsonPath().getList("schemas"), contains("urn:ietf:params:scim:schemas:core:2.0:Group"));
-        assertThat(response.jsonPath().getString("displayName"), is(name));
     }
 }
